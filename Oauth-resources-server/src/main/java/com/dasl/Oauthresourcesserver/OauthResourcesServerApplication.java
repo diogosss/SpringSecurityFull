d964997c936c47cee6346e7b3f275f9f7d424e98
Diego Salazar <diogol11ldu20@hotmail.com>
2022-06-24 17:50:33 -0500

    [First] Creating OAuth Resource Server, Configuring OAuth Resource Server

---
+++ Oauth-resources-server/src/main/java/com/dasl/Oauthresourcesserver/OauthResourcesServerApplication.java
@@ -0,0 +1,13 @@
+package com.dasl.Oauthresourcesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthResourcesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthResourcesServerApplication.class, args);
+	}
+
+}
---
+++ Oauth-resources-server/src/test/java/com/dasl/Oauthresourcesserver/OauthResourcesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasl.Oauthresourcesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthResourcesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
